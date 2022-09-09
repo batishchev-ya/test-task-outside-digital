b7b859aaaaa6af3d6f29b02f9814d20ed3ea2b08
Yaroslav Batushchev <yaroslav.batishchev1@gmail.com>
2022-09-09 22:49:08 +0300

    added remote db

--- db/index.js
+++ db/index.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: process.env.DB_USERNAME,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_STRING,
 });
 
 module.exports = {
